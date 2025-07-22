60561ee3d60a8a87cca240643953fb9fd0d67cbd
oghenetega16 <sukuruoghenetega@gmail.com>
2025-07-22 11:24:39 +0100

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Form from './Form'
 
 export default function App() {
     return (
-      <main className="min-h-screen bg-red text-white font-display bg-[url('./assets/images/bg-intro-mobile.png')] bg-no-repeat bg-cover md:bg-[url('./assets/images/bg-intro-desktop.png')] px-6 py-24 flex flex-col md:flex-row md:items-center lg:justify-center lg:gap-10">
+      <main className="min-h-screen text-white font-display bg-[url('./assets/images/bg-intro-mobile.png')] bg-no-repeat bg-cover md:bg-[url('./assets/images/bg-intro-desktop.png')] px-6 py-24 flex flex-col md:flex-row md:items-center lg:justify-center lg:gap-10">
         <Home />
         <Form />
       </main>
